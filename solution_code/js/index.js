4bb137eaad614ae9cab8b1a08ee20f5797d88931
Joni Halabi <her@thatdevgirl.com>
2016-04-12 01:16:41 -0400

    Feedback on homework #4

--- solution_code/js/index.js
+++ solution_code/js/index.js
@@ -1,3 +1,12 @@
+/* 
+	Hi Brent!  I added feedback about your homework in Javascript comments like this one.
+
+	Overall, fantastic job!  Your code is super clean and organized, and I love the comments 
+	throughout the file describing each function.
+
+	More feedback below.
+*/
+
 $(document).ready(function () {
 
 	/* Set all of the event handlers */
@@ -9,6 +18,19 @@ $(document).ready(function () {
 
 	/* Function - Slide down to show more of the article on click of "read more"
 	button */
+
+	/* 
+		One note (that I haven't gone over yet, but will in Tuesday's class) about event.preventDefault:
+		Some browsers will produe an error if you do not pass "event" into the function.  This doesn't
+		happen in Chrome, but I have seen errors in Firefox where it will not recognize "event".  
+
+		You just need to add it between the parenthesis after the function's name:
+
+		function showReadMoreText(event) {
+			// all the rest of your code.
+		}
+	*/
+
 	function showReadMoreText () {
 		event.preventDefault();
 		$('.readmore').hide();
@@ -22,6 +44,14 @@ $(document).ready(function () {
 		event.preventDefault();
 		/* Selecting the callback argument for slideUp() allows for a smoother 
 	    transistion with hiding/showing the Read Less/More text */
+
+	    /* 
+	    	Excellent use of the callback here!!  
+
+	    	You can also use it in the showReadMoreText() function above - having that consistency 
+	    	in your code of how you generally handle things is better form.
+	    */
+	    
 		$('#show-this-on-click').slideUp(function () {
 			$('.readless').hide();
 			$('.readmore').show();
